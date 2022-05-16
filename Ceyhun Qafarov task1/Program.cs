38bf44de8dea7a03f21c20c13a8880dc5715ae2b
*jeyhun <*jeyhunag@code.edu.az*>
2022-05-17 00:15:13 +0300

    1

---
+++ Ceyhun Qafarov task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Ceyhun_Qafarov_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random blur = new Random();
+            int a = blur.Next(0, 100);
+            int texmin;
+            do
+            {
+
+                Console.WriteLine("bir reqem daxil edin");
+                texmin = int.Parse(Console.ReadLine());
+                if (texmin == a)
+                {
+                    Console.WriteLine("Tebrik edirik....");
+                }
+                if (texmin < a)
+                {
+                    Console.WriteLine("Yuxari");
+
+                }
+                if (texmin > a)
+                {
+                    Console.WriteLine("Asagi");
+                }
+
+            } while (texmin != a);
+
+        }
+    }
+}
